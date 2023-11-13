087e7b244c651530c39f1ec65fa7b0e8c26a8cd4
neddington <neddington@cascadia.edu>
2023-11-13 15:51:12 -0800

    Initial Commit

---
+++ Detecting Motion and Orientation/Detecting_Motion_and_OrientationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Detecting_Motion_and_OrientationApp.swift
+//  Detecting Motion and Orientation
+//
+//  Created by Eddington, Nick on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Detecting_Motion_and_OrientationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
